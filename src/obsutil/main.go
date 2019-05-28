4303b6b8f439713e7b1d271dced435433aee9abb
chewaiwai <51107690+chewaiwai@users.noreply.github.com>
2019-05-28 16:54:35 +0800

    Update main.go

--- src/obsutil/main.go
+++ src/obsutil/main.go
@@ -1,11 +1,19 @@
 package main
 
 import (
+	"assist"
 	"command"
 	"runtime"
 )
 
+var (
+	AesKey    = "not set"
+	AesIv     = "not set"
+	CloudType = ""
+)
+
 func main() {
 	runtime.GOMAXPROCS(runtime.NumCPU())
-	command.Run()
+	assist.SetCloudType(CloudType)
+	command.Run(AesKey, AesIv)
 }
